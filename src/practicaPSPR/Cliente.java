bd7accc80b40b6a75552a54557c574cf14fd4c28
sergiiomg <sergiiomgdaw@gmail.com>
2025-11-26 13:30:47 +0100

    Clase cliente

--- src/practicaPSPR/Cliente.java
+++ src/practicaPSPR/Cliente.java
@@ -1,5 +1,29 @@
 package practicaPSPR;
 
-public class Cliente {
+import java.util.ArrayList;
 
+public class Cliente extends Thread {
+
+    private ArrayList<Integer> barra;
+    private int iteraciones;
+    private int idCliente;
+
+    public Cliente(ArrayList<Integer> barra, int iteraciones, int idCliente) {
+        this.barra = barra;
+        this.iteraciones = iteraciones;
+        this.idCliente = idCliente;
+    }
+
+    public void run() {
+        for (int i = 1; i <= iteraciones; i++) {
+            if (!barra.isEmpty()) {
+                int num = barra.remove(0);
+                System.out.println("Cliente " + idCliente + " bebió el culin " + num);
+            }
+            try {
+                Thread.sleep((int)(Math.random() * 250));
+            } catch (InterruptedException e) {}
+        }
+    }
 }
+
